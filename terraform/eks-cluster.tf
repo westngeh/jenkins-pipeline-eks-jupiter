858524ef3b3bb73d8ff5072f015da8939d2b7108
elvis.ngeh <evis.bantar@gmail.com>
2024-06-26 22:58:38 -0400

    modified vpc & eks versions

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
+    version = "~> 20.14.0"
     cluster_name = "myapp-eks-cluster"
     cluster_version = "1.24"
 
